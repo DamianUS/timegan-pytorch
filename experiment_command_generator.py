6116d4df432b4ca8a1037511124de8f805171ec5
alejandrofdez-us <afdez@us.es>
2022-11-27 13:32:02 +0100

    Experiments defined

--- experiment_command_generator.py
+++ experiment_command_generator.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 def generate_experiment_command(module, num_layers, hidden_dim, emb_epochs, sup_epochs, gan_epochs, learning_rate,
                                 trace, seq_len, batch_size, scaling_method):
-    experiment_save_dir = './experiments/timegan/alibaba2018/ecms-experiments-test'
+    experiment_save_dir = f'./experiments/timegan-pytorch/alibaba2018/ecms-experiments-pytorch/gan_epochs-{gan_epochs}-num-layers-{num_layers}-hidden_dim-{hidden-dim}-batch_size-{batch_size}'
     return f'python main.py --module {module} --num_layers {num_layers} --hidden_dim {hidden_dim} --emb_epochs {emb_epochs} ' \
            f'--sup_epochs {sup_epochs} --gan_epochs {gan_epochs} --learning_rate {learning_rate} ' \
            f'--trace {trace} --seq_len {seq_len} --batch_size {batch_size} --n_samples 10 --scaling_method {scaling_method} ' \
@@ -14,16 +14,16 @@ def generate_experiment_command(module, num_layers, hidden_dim, emb_epochs, sup_
            f'\n'
 
 modules = ['gru']
-num_layers = [3]
-hidden_dims = [10]
-emb_epochs = [500]
-sup_epochs = [500]
-gan_epochs = [500]
+num_layers = [2,3,4]
+hidden_dims = [5,10,15]
+emb_epochs = [200]
+sup_epochs = [200]
+gan_epochs = [200]
 learning_rates = [1e-3]
 
 traces = ['alibaba2018']
 seq_lengths = [288]
-batch_sizes = [100]
+batch_sizes = [32,64,128]
 scaling_methods = ['minmax']  # 'minmax'
 
 parameterization = [modules, num_layers, hidden_dims, emb_epochs, sup_epochs, gan_epochs, learning_rates, traces,
