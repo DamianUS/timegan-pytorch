959392f7beeffca420dd4622f16b1cd10157b67a
alejandrofdez-us <afdez@us.es>
2022-12-01 18:51:01 +0100

    Fixed bug

--- experiment_command_generator.py
+++ experiment_command_generator.py
@@ -7,7 +7,7 @@ from datetime import datetime
 def generate_experiment_command(module, num_layers, hidden_dim, emb_epochs, sup_epochs, gan_epochs, embedding_dropout,
                     recovery_dropout, supervisor_dropout, generator_dropout, discriminator_dropout, learning_rate,
                                 trace, seq_len, batch_size, scaling_method):
-    experiment_save_dir = '~/experiments/timegan-pytorch/alibaba2018/small-tests/test/tensorboard/num_layers-{num_layers}-hidden_dim-{hidden_dim}-gan_epochs-{gan_epochs}-batch_size-{batch_size}'
+    experiment_save_dir = f'~/experiments/timegan-pytorch/alibaba2018/small-tests/test/tensorboard/num_layers-{num_layers}-hidden_dim-{hidden_dim}-gan_epochs-{gan_epochs}-batch_size-{batch_size}'
     return f'python main.py --module {module} --num_layers {num_layers} --hidden_dim {hidden_dim} --emb_epochs {emb_epochs} ' \
            f'--sup_epochs {sup_epochs} --gan_epochs {gan_epochs} --embedding_dropout {embedding_dropout} --recovery_dropout {recovery_dropout} ' \
            f'--supervisor_dropout {supervisor_dropout} --generator_dropout {generator_dropout} --discriminator_dropout {discriminator_dropout} --learning_rate {learning_rate} ' \
