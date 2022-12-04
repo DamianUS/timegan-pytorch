aaf2fa3513681ba2976dd6f428fe00b62b8a2f44
alejandrofdez-us <afdez@us.es>
2022-12-04 17:17:11 +0100

    First version of parallel

--- predict.py
+++ predict.py
@@ -10,6 +10,12 @@ from tqdm import tqdm
 from natsort import natsorted
 
 
+from concurrent.futures import ProcessPoolExecutor
+
+MAX_WORKERS = int(ProcessPoolExecutor()._max_workers/2)
+CHUNK_SIZE = 5
+
+
 def main(args):
     print(args)
     experiment_directories = []
@@ -24,45 +30,62 @@ def main(args):
         experiment_directories.append(f'{root_dir}/epoch_{args.epoch}')
     else:
         experiment_directories.append(root_dir)
-    progress_bar = tqdm(experiment_directories)
-    for experiment_dir in progress_bar:
-        progress_bar.set_description(f'Generating samples with model at {os.path.basename(os.path.normpath(experiment_dir))}')
-        with open(f"{experiment_dir}/args.pickle", "rb") as fb:
-            recovered_args = torch.load(fb)
-        with open(f"{experiment_dir}/model.pt", "rb") as fb:
-            recovered_model_state_dict = torch.load(fb)
-        recovered_args.experiment_save_dir = f'{experiment_dir}'
-        recovered_args.is_train = False
-        recovered_args.n_samples = args.n_samples_export
-        recovered_args.max_seq_len = recovered_args.seq_len
-        recovered_args.model_path = experiment_dir
-        if not hasattr(recovered_args, 'recovery_sigmoid'):
-            recovered_args.recovery_sigmoid = args.recovery_sigmoid
-        if not hasattr(recovered_args, 'embedding_dropout'):
-            recovered_args.embedding_dropout = 0.0
-        if not hasattr(recovered_args, 'recovery_dropout'):
-            recovered_args.recovery_dropout = 0.0
-        if not hasattr(recovered_args, 'supervisor_dropout'):
-            recovered_args.supervisor_dropout = 0.0
-        if not hasattr(recovered_args, 'generator_dropout'):
-            recovered_args.generator_dropout = 0.0
-        if not hasattr(recovered_args, 'discriminator_dropout'):
-            recovered_args.discriminator_dropout = 0.0
+    #progress_bar = tqdm()
+    args_params_array = []
+    for experiment_dir in experiment_directories:
+        args.experiment_dir = experiment_dir
+        args_params_array.append(copy.deepcopy(args))
 
-        # TODO: Fix scaler
-        model = TimeGAN(recovered_args)
-        model.load_state_dict(recovered_model_state_dict)
-        if recovered_args.ori_data_filename is not None:
-            X, T, scaler = data_load.get_dataset(ori_data_filename=recovered_args.ori_data_filename, sequence_length=recovered_args.seq_len,
-                                                 stride=1, trace_timestep=recovered_args.trace_timestep, shuffle=False, seed=13,
-                                                 scaling_method='minmax')
-        else:
-            X, T, scaler = data_load.get_datacentertraces_dataset(trace=recovered_args.trace, trace_type=recovered_args.trace_type,
-                                                                  sequence_length=recovered_args.seq_len, stride=1,
-                                                                  trace_timestep=recovered_args.trace_timestep, shuffle=False,
-                                                                  seed=13, scaling_method='minmax')
-        generated_data = timegan_generator(model, T, recovered_args)
-        save_generated_data(generated_data, scaler, f'{experiment_dir}/generated_data', n_samples=10)
+    with multiprocessing.Pool(processes=MAX_WORKERS) as pool:
+        results_progress_bar = tqdm(
+            results_progress_bar = pool.imap_unordered(load_and_generate_samples, args_params_array, chunksize=CHUNK_SIZE),
+            total=len(args_params_array),
+            desc='Computing metrics'
+        )
+    for generated_data, scaler, experiment_dir in results_progress_bar:
+            save_generated_data(generated_data, scaler, f'{experiment_dir}/generated_data', n_samples=10)
+
+
+def load_and_generate_samples(args):
+    with open(f"{args.experiment_dir}/args.pickle", "rb") as fb:
+        recovered_args = torch.load(fb)
+    with open(f"{args.experiment_dir}/model.pt", "rb") as fb:
+        recovered_model_state_dict = torch.load(fb)
+    recovered_args.experiment_save_dir = f'{args.experiment_dir}'
+    recovered_args.is_train = False
+    recovered_args.n_samples = args.n_samples_export
+    recovered_args.max_seq_len = recovered_args.seq_len
+    recovered_args.model_path = args.experiment_dir
+    if not hasattr(recovered_args, 'recovery_sigmoid'):
+        recovered_args.recovery_sigmoid = args.recovery_sigmoid
+    if not hasattr(recovered_args, 'embedding_dropout'):
+        recovered_args.embedding_dropout = 0.0
+    if not hasattr(recovered_args, 'recovery_dropout'):
+        recovered_args.recovery_dropout = 0.0
+    if not hasattr(recovered_args, 'supervisor_dropout'):
+        recovered_args.supervisor_dropout = 0.0
+    if not hasattr(recovered_args, 'generator_dropout'):
+        recovered_args.generator_dropout = 0.0
+    if not hasattr(recovered_args, 'discriminator_dropout'):
+        recovered_args.discriminator_dropout = 0.0
+    # TODO: Fix scaler
+    model = TimeGAN(recovered_args)
+    model.load_state_dict(recovered_model_state_dict)
+    if recovered_args.ori_data_filename is not None:
+        X, T, scaler = data_load.get_dataset(ori_data_filename=recovered_args.ori_data_filename,
+                                             sequence_length=recovered_args.seq_len,
+                                             stride=1, trace_timestep=recovered_args.trace_timestep, shuffle=False,
+                                             seed=13,
+                                             scaling_method='minmax')
+    else:
+        X, T, scaler = data_load.get_datacentertraces_dataset(trace=recovered_args.trace,
+                                                              trace_type=recovered_args.trace_type,
+                                                              sequence_length=recovered_args.seq_len, stride=1,
+                                                              trace_timestep=recovered_args.trace_timestep,
+                                                              shuffle=False,
+                                                              seed=13, scaling_method='minmax')
+    generated_data = timegan_generator(model, T, recovered_args)
+    return generated_data, scaler, args.experiment_dir
 
 
 if __name__ == '__main__':
